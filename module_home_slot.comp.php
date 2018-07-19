df2a8f0855ba5474645d466e91b1bdcd9b124909
TWAHIRWA <twahirwa@outlook.fr>
2018-07-19 17:29:15 +0200

    Add files via upload

--- module_home_slot.comp.php
+++ module_home_slot.comp.php
@@ -80,18 +80,22 @@ if(is_array($slot6) && count($slot5) > 0) {
 .feature-velocityx { background-image: linear-gradient(rgba(0, 0, 0, .7), rgba(0, 0, 0, .1)), url('/data/feature-module/<?php echo $slot6->getPictureHomeSlot();?>'); }
 
 
-     .tile { padding: 3em 2em 2em 2em; height: 300px; background-color: rgba(15,15,15,0.5); background-position: center; background-repeat: no-repeat; background-size: cover; } .tile:hover { cursor: pointer } a.tile-button { background-color: rgba(100, 100, 100, .7); font-weight: 700; color: #fff; padding: 1em; text-transform: uppercase; font-size: .8em; } a.tile-button:hover { background-color: rgba(255, 255, 255, 1); color: #000; font-weight: 700; }
-     .tile h3 { font-size: 1.5em; color: #fff; margin: 0; }
-     .landing-section .column { transition: all .2s ease-in }
+     .tile { padding: 3em 2em 2em 2em; height: 300px; background-color: rgba(15,15,15,0.5); background-position: center; background-repeat: no-repeat; background-size: cover; }
+
+      .tile:hover { cursor: pointer } a.tile-button { background-color: rgba(100, 100, 100, .7); font-weight: 700; color: #fff; padding: 1em; text-transform: uppercase; font-size: .8em; }
+      a.tile-button:hover { background-color: rgba(255, 255, 255, 1); color: #000; font-weight: 700; }
+     .tile
+
+     h3 { font-size: 1.5em; color: #fff; margin: 0; max-}
+     .landing-section
+
      .product-icon { width: 150px; margin-bottom: 10px; }
-      /* COLORS */ .quardro-light-blue-text { color: #48a8d2 }
+      .quardro-light-blue-text { color: #48a8d2 }
        .quardro-light-blue-fill { background-color: #48a8d2 }
         .quardro-dark-blue-text { color: #005f88 }
         .quardro-dark-blue-fill { background-color: #005f88 }
         .quadro-green { color: #82a058; }
          .quadro-green-fill { background-color: #82a058; }
-          .hvr-underline-center { position: relative; display: inline-block; vertical-align: middle; overflow: hidden; box-shadow: 0 0 1px transparent; -webkit-transform: perspective(1px) translateZ(0); transform: perspective(1px) translateZ(0); } .hvr-underline-center:before { content: ""; position: absolute; right: 50%; bottom: 0; left: 50%; z-index: -1; height: 4px; background: #48a8d2; -webkit-transition-property: left, right; transition-property: left, right; -webkit-transition-duration: 0.3s; transition-duration: 0.3s; -webkit-transition-timing-function: ease-out; transition-timing-function: ease-out; }
-          .hvr-underline-center:hover:before, .hvr-underline-center:focus:before, .hvr-underline-center:active:before { right: 0; left: 0; } /* Overline Reveal */
 
 
 
@@ -102,11 +106,6 @@ if(is_array($slot6) && count($slot5) > 0) {
            .hvr-overline-quadro:hover:before, .hvr-overline-quadro:focus:before, .hvr-overline-quadro:active:before { -webkit-transform: translateY(0); transform: translateY(0); }
 
 
-
-
-
-
-
       .hvr-overline-xlr8 { position: relative; display: inline-block; vertical-align: middle; overflow: hidden; box-shadow: 0 0 1px transparent; -webkit-transform: perspective(1px) translateZ(0); transform: perspective(1px) translateZ(0); }
 
        .hvr-overline-xlr8:before { content: ""; position: absolute; top: 0; right: 0; left: 0; z-index: -1; height: 20px; background: #9E0B0F; -webkit-transition-property: transform; transition-property: transform; -webkit-transition-duration: 0.3s; transition-duration: 0.3s; -webkit-transition-timing-function: ease-out; transition-timing-function: ease-out; -webkit-transform: translateY(-20px); transform: translateY(-20px); }
@@ -115,21 +114,37 @@ if(is_array($slot6) && count($slot5) > 0) {
 
 
 
+      .column { transition: all .2s ease-in;max-width: 100%; max-height:auto;
+        }
 
 
-        .hvr-overline-destiny2 { position: relative; display: inline-block; vertical-align: middle; overflow: hidden; box-shadow: 0 0 1px transparent; -webkit-transform: perspective(1px) translateZ(0); transform: perspective(1px) translateZ(0); }
+@media screen and (max-width: 64em) {
+.tile h3 {
+    font-size: 16px;
+  }
+}
 
-         .hvr-overline-destiny2:before { content: ""; position: absolute; top: 0; right: 0; left: 0; z-index: -1; height: 20px; background: #008fd5; -webkit-transition-property: transform; transition-property: transform; -webkit-transition-duration: 0.3s; transition-duration: 0.3s; -webkit-transition-timing-function: ease-out; transition-timing-function: ease-out; -webkit-transform: translateY(-20px); transform: translateY(-20px); }
+@media screen and (max-width: 50em) {
+.tile h3 {
+    font-size: 14px;
+  }
+}
+
+@media screen and (max-width: 30em){
+.tile h3 {
+    font-size: 12px;
+  }
+}
+
+
 
 </style>
 
 
 
-
-
-
+<div style="max-width: 100%;">
 <a class="" href="<?php echo $slot3->getLinkHomeSlot();?>">
-<div class="tile feature-actuPro sm-12 med-12 lg-6 column hvr-overline-quadro">
+<div class="tile feature-actuPro sm-36 med-24 lg-6   column hvr-overline-quadro">
 <h3><?php echo stripslashes($slot3->getTextHomeSlot($language, DEFAULT_LANGUAGE));?></h3>
 <br>
 <a class="tile-button" href="<?php echo $slot3->getLinkHomeSlot();?>"><?php echo mb_strtoupper(toolsPny::getStaticRessource('btn_learn_more', $language, $tabLabelsFront), 'utf8') ; ?></a>
@@ -138,9 +153,8 @@ if(is_array($slot6) && count($slot5) > 0) {
 
 
 
-
 <a class="" href="<?php echo $slot2->getLinkHomeSlot();?>">
-<div class="tile feature-pnypro col-sm-9 hvr-overline-quadro">
+<div class="tile feature-pnypro sm-36 med-36 lg-18  column hvr-overline-quadro">
 <h3><?php echo stripslashes($slot2->getTextHomeSlot($language, DEFAULT_LANGUAGE));?></h3>
 <br>
 <a class="tile-button" href="<?php echo $slot2->getLinkHomeSlot();?>"><?php echo mb_strtoupper(toolsPny::getStaticRessource('btn_learn_more', $language, $tabLabelsFront), 'utf8') ; ?></a>
@@ -150,7 +164,7 @@ if(is_array($slot6) && count($slot5) > 0) {
 
 
 <a class="" href="<?php echo $slot5->getLinkHomeSlot();?>">
-<div class="tile feature-OTG-Micro-USB sm-12 med-12 lg-6 column hvr-overline-quadro">
+<div class="tile feature-OTG-Micro-USB sm-12 med-12 lg-6  column hvr-overline-quadro">
 <h3><?php echo stripslashes($slot1->getTextHomeSlot($language, DEFAULT_LANGUAGE));?></h3>
 <br>
 <a class="tile-button" href="<?php echo $slot1->getLinkHomeSlot();?>"><?php echo mb_strtoupper(toolsPny::getStaticRessource('btn_learn_more', $language, $tabLabelsFront), 'utf8') ; ?></a>
@@ -161,7 +175,7 @@ if(is_array($slot6) && count($slot5) > 0) {
 
 
 <a class="" href="<?php echo $slot5->getLinkHomeSlot();?>">
-<div class="tile feature-xlr8 sm-12 med-12 lg-6 column hvr-overline-xlr8">
+<div class="tile feature-xlr8 sm-12 med-12 lg-6  column hvr-overline-xlr8">
 <h3><?php echo stripslashes($slot5->getTextHomeSlot($language, DEFAULT_LANGUAGE));?></h3>
 <br>
 <a class="tile-button" href="<?php echo $slot5->getLinkHomeSlot();?>"><?php echo mb_strtoupper(toolsPny::getStaticRessource('btn_learn_more', $language, $tabLabelsFront), 'utf8') ; ?></a>
@@ -173,7 +187,7 @@ if(is_array($slot6) && count($slot5) > 0) {
 
 
 <a class="" href="<?php echo $slot4->getLinkHomeSlot();?>">
-<div class="tile feature-ambassador sm-12 med-12 lg-6 column hvr-overline-xlr8">
+<div class="tile feature-ambassador sm-12 med-12 lg-6  column hvr-overline-xlr8">
 <h3><?php echo stripslashes($slot4->getTextHomeSlot($language, DEFAULT_LANGUAGE));?></h3>
 <br>
 <a class="tile-button" href="<?php echo $slot4->getLinkHomeSlot();?>"><?php echo mb_strtoupper(toolsPny::getStaticRessource('btn_learn_more', $language, $tabLabelsFront), 'utf8') ; ?></a>
@@ -181,30 +195,13 @@ if(is_array($slot6) && count($slot5) > 0) {
 </a>
 
 
-<!--
-<a class="" href="<?php echo $slot5->getLinkHomeSlot();?>">
-<div class="tile feature-xlr8 sm-12 med-12 lg-6 column hvr-overline-xlr8">
-<h3><?php echo stripslashes($slot5->getTextHomeSlot($language, DEFAULT_LANGUAGE));?></h3>
-<br>
-<a class="tile-button" href="<?php echo $slot5->getLinkHomeSlot();?>"><?php echo mb_strtoupper(toolsPny::getStaticRessource('btn_learn_more', $language, $tabLabelsFront), 'utf8') ; ?></a>
-</div>
-</a>
-
-
-<a class="" href="<?php echo $slot5->getLinkHomeSlot();?>">
-<div class="tile feature-xlr8 sm-12 med-12 lg-6 column hvr-overline-xlr8">
-<h3><?php echo stripslashes($slot5->getTextHomeSlot($language, DEFAULT_LANGUAGE));?></h3>
-<br>
-<a class="tile-button" href="<?php echo $slot5->getLinkHomeSlot();?>"><?php echo mb_strtoupper(toolsPny::getStaticRessource('btn_learn_more', $language, $tabLabelsFront), 'utf8') ; ?></a>
-</div>
-</a>-->
-
-
-
 <a class="" href="<?php echo $slot6->getLinkHomeSlot();?>">
-<div class="tile feature-velocityx sm-12 med-12 lg-6 column hvr-overline-quadro">
+<div class="tile feature-velocityx sm-12 med-12 lg-6   column hvr-overline-quadro">
 <h3><?php echo stripslashes($slot6->getTextHomeSlot($language, DEFAULT_LANGUAGE));?></h3>
 <br>
 <a class="tile-button" href="<?php echo $slot6->getLinkHomeSlot();?>"><?php echo mb_strtoupper(toolsPny::getStaticRessource('btn_learn_more', $language, $tabLabelsFront), 'utf8') ; ?></a>
 </div>
 </a>
+</div>
+
+<div class="clear-both"></div>
