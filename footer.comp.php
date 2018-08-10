14d3ba4413732e0a897bc68928341e8a0fa44074
TWAHIRWA <twahirwa@outlook.fr>
2018-08-10 17:11:20 +0200

    Add files via upload

--- footer.comp.php
+++ footer.comp.php
@@ -1,109 +1,82 @@
-<link rel="stylesheet" type="text/css" href="/design/css/footer.css">
-<style type="text/css">
+            <link rel="stylesheet" type="text/css" href="/design/css/footer.css">
 
 
 
-.row {
-
-    margin-left: auto;
-    margin-right: auto;
-    margin-top: 0;
-    margin-bottom: 0;
-    max-width: 80em;
-    position: relative;
-    font-size: 12px;
-
-}
 
 
-</style>
+<footer class="main-footer" role="contentinfo">
 
-<div class="footer-top-bar"></div>
+    <div class="footer-top-bar"></div>
 
 
-                 <div style="  background: #454545;" class="footer-padd">
- <div class="footer-banner">
-
+     <div class="footer-banner">
         <div class="social-foot-section row">
-<div class="lg-8 column no-padd pnycompany">
-<h5 class="social-header">PNY Europe</h5>
-<a href="https://www.facebook.com/pnyeuropemea"><img class="pny-icon" src="/data/social-medias/facebook.svg" alt="Facebook"></a>
-<a href="https://twitter.com/pnyfrance"><img class="pny-icon" src="/data/social-medias/twitter.svg" alt="Twitter"></a>
-<a href="https://www.youtube.com/pnytecheumea"><img class="pny-icon" src="/data/social-medias/youtube.svg" alt="Youtube"></a>
-<a href="https://www.linkedin.com/company/pny-technologies-europe"><img class="pny-icon" src="/data/social-medias/linkedin.svg" alt="linked-in"></a>
-<a href="https://instagram.com/pnytecheurope/"><img alt="" class="pny-icon" src="/data/social-medias/instagram.svg"></a>
-</div>
-<div class="lg-8 column no-padd pro">
-<h5 class="social-header">PNY Europe PRO</h5>
-<a href="http://www.twitter.com/pnyprosolutions"><img alt="" class="pnypro-icon" src="/data/social-medias/pnypro-twitter.svg"></a>
-</div>
-<div class="lg-8 column no-padd">
-<h5 class="social-header">XLR8 GAMING</h5>
-<a href="https://facebook.com/xlr8gam1ng"><img alt="" class="pny-icon" src="/data/social-medias/xlr8-facebook.svg"></a>
-<a href="https://twitter.com/xlr8gam1ng"><img alt="" class="pny-icon" src="/data/social-medias/xlr8-twitter.svg"></a>
-<a href="https://instagram.com/xlr8gam1ng"><img alt="" class="pny-icon" src="/data/social-medias/xlr8-instagram.svg"></a>
-<a href="https://www.twitch.com/xlr8gam1ng/"><img alt="" class="pny-icon" src="/data/social-medias/xlr8-twitch.svg"></a>
-</div>
-</div>
-    </div>
-
-
-
-
-<div class="lower-footer row">
-
-                    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-<div class="row">
-<div class="x-sm-12 sm-6 med-4 lg-4 column"> <nav> <a class="header" href="/support/technical-support"><em aria-hidden="true" class="fa fa-wrench"></em> Technical Support</a> </nav>
-<ul>
-   <!-- <li>     <a href="tel:+18002344597"><em aria-hidden="true" class="fa fa-phone"></em> 1-800-234-4597</a>     </li>-->
-    <li>     <a href="/support/support-form"><em aria-hidden="true" class="fa fa-envelope"></em> <?php echo toolsPny::getStaticRessource('contact_us', $language, $tabLabelsFront) ; ?></a>     </li>
-</ul>
-</div>
-<div class="x-sm-12 sm-6 med-4 lg-4 column"> <nav> <a class="header" href="/support/technical-support"><em aria-hidden="true" class="fa fa-shopping-cart"></em> Customer  Support</a> </nav>
-<ul>
-   <!-- <li>     <a href="tel:+18007690143"><em aria-hidden="true" class="fa fa-phone"></em> 1-800-769-0143</a>     </li>-->
-    <li>     <a target="_self" href="mailto:sales@pny.eu" class="block gold-icon"><em aria-hidden="true" class="fa fa-envelope"></em> <?php echo toolsPny::getStaticRessource('email_us', $language, $tabLabelsFront) ; ?></a>     </li>
-</ul>
-</div>
-<div class="x-sm-12 sm-6 med-4 lg-4 column"> <nav> <a class="header" href="/support/technical-support"><em aria-hidden="true" class="fa fa-headphones"></em> Support</a> </nav>
-<ul>
-    <!--<li>     <a href="/promo/product-registration">Product Registration</a></li>-->
-    <!--<li>     <a href="/support/rebates">Rebates</a>     </li>-->
-    <li>     <a href="/support/glossary"><?php echo toolsPny::getStaticRessource('support_glossary', $language, $tabLabelsFront) ; ?></a>     </li>
-    <li>     <a href="/support/contact-us"><?php echo toolsPny::getStaticRessource('contact_us', $language, $tabLabelsFront) ; ?></a>     </li>
-    <!--<li>     <a href="/support-footer/product-recall">Product Recall</a>     </li>-->
-    <!--<li>     <a href="/mega-consumer/geforce-legacy">GeForce Legacy</a>     </li>-->
-</ul>
-</div>
-<div class="x-sm-12 sm-6 med-4 lg-4 column"> <nav> <a class="header" href="/company/about-pny"><em aria-hidden="true" class="fa fa-building"></em> Company</a> </nav>
-<ul>
-    <li>     <a href="/company/about-pny"><?php echo toolsPny::getStaticRessource('company_about', $language, $tabLabelsFront) ; ?></a>     </li>
-    <li>     <a href="/company/press-center">Press Center</a>     </li>
-    <li>     <a href="/company/press-center/events"><?php echo toolsPny::getStaticRessource('events', $language, $tabLabelsFront) ; ?></a>     </li>
-    <li>     <a href="/company/careers"><?php echo toolsPny::getStaticRessource('company_careers', $language, $tabLabelsFront) ; ?></a>     </li>
-    <li>     <a href="/company/where-to-buy"><?php echo toolsPny::getStaticRessource('where_to_buy', $language, $tabLabelsFront) ; ?></a>     </li>
-    <li>     <a href="/company/contact-us"><?php echo toolsPny::getStaticRessource('contact_us', $language, $tabLabelsFront) ; ?></a>     </li>
-</ul>
-</div>
-<div class="x-sm-12 sm-6 med-4 lg-4 column"> <nav> <a class="header" href="/company/about-pny"><em aria-hidden="true" class="fa fa-file-text-o"></em> Legal</a> </nav>
-<ul>
-    <li>     <a href="/legal/privacy"><?php echo toolsPny::getStaticRessource('your_privacy', $language, $tabLabelsFront) ; ?></a>     </li>
-    <li>     <a href="/legal/terms-of-use"><?php echo toolsPny::getStaticRessource('terms_of_use', $language, $tabLabelsFront) ; ?></a>     </li>
-   <!-- <li>     <a href="/support/order-support/terms-of-sale"><?php echo toolsPny::getStaticRessource('contact_us', $language, $tabLabelsFront) ; ?></a>     </li>-->
-    <!--<li>     <a href="/legal/disclaimer-third-party-links">Disclaimer &amp; Third-Party Links</a>     </li>-->
-</ul>
-</div>
-<div class="clear-both">&nbsp;</div>
-<div class="sm-12 med-12 lg-12 column">
-<p class="footnote"> Copyright © <?php echo date('Y');?> PNY Technologies Inc. All rights reserved. </p>
-</div>
-<!--
-<div class="x-sm-8 sm-4 med-4 lg-3 column push-right-lg text-right"><img alt="HP Business Partner" class="footer-icon" src="/File%20Library/Logos/hp-business-partner.svg"></div>
-<div class="x-sm-8 sm-4 med-4 lg-3 column push-right-lg text-right"><img alt="Keeping and Creating Jobs in the USA" class="footer-icon" src="/File%20Library/Logos/keeping-and-creating-jobs-in-the-usa.svg"></div>
-<div class="x-sm-8 sm-4 med-4 lg-3 column push-right-lg text-right"><img alt="Powered by iApps" class="footer-icon" src="/File%20Library/Logos/powered-by-iapps.svg"></div>
--->
-                    </div>
+                <div class="lg-8 column no-padd pnycompany">
+                    <h5 class="social-header">PNY Europe</h5>
+                    <a href="https://www.facebook.com/pnyeuropemea"><img class="pny-icon" src="/data/social-medias/facebook.svg" alt="Facebook"></a>
+                    <a href="https://twitter.com/pnyfrance"><img class="pny-icon" src="/data/social-medias/twitter.svg" alt="Twitter"></a>
+                    <a href="https://www.youtube.com/pnytecheumea"><img class="pny-icon" src="/data/social-medias/youtube.svg" alt="Youtube"></a>
+                    <a href="https://www.linkedin.com/company/pny-technologies-europe"><img class="pny-icon" src="/data/social-medias/linkedin.svg" alt="linked-in"></a>
+                    <a href="https://instagram.com/pnytecheurope/"><img alt="" class="pny-icon" src="/data/social-medias/instagram.svg"></a>
                 </div>
-            </div>     </div>   </footer>
+                <div class="lg-8 column no-padd pro">
+                    <h5 class="social-header">PNY Europe PRO</h5>
+                    <a href="http://www.twitter.com/pnyprosolutions"><img alt="" class="pnypro-icon" src="/data/social-medias/pnypro-twitter.svg"></a>
+                </div>
+                <div class="lg-8 column no-padd">
+                    <h5 class="social-header">XLR8 GAMING</h5>
+                    <a href="https://facebook.com/xlr8gam1ng"><img alt="" class="pny-icon" src="/data/social-medias/xlr8-facebook.svg"></a>
+                    <a href="https://twitter.com/xlr8gam1ng"><img alt="" class="pny-icon" src="/data/social-medias/xlr8-twitter.svg"></a>
+                    <a href="https://instagram.com/xlr8gam1ng"><img alt="" class="pny-icon" src="/data/social-medias/xlr8-instagram.svg"></a>
+                    <a href="https://www.twitch.com/xlr8gam1ng/"><img alt="" class="pny-icon" src="/data/social-medias/xlr8-twitch.svg"></a>
+                </div>
+            </div>
+        </div>
+
+
+
+
+            <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+            <div class="row">
+                <div class="lower-footer x-sm-12 sm-6 med-4 lg-4 column"> <nav> <a class="header" href="/support/technical-support"><em aria-hidden="true" class="fa fa-wrench"></em> <?php echo toolsPny::getStaticRessource('technical_support', $language, $tabLabelsFront) ; ?></a> </nav>
+                    <ul>
+                        <li>     <a href="/support/support-form"><em aria-hidden="true" class="fa fa-envelope"></em> <?php echo toolsPny::getStaticRessource('contact_us', $language, $tabLabelsFront) ; ?></a>     </li>
+                    </ul>
+                </div>
+                <div class=" lower-footer x-sm-12 sm-6 med-4 lg-4 column"> <nav> <a class="header" href="/support/technical-support"><em aria-hidden="true" class="fa fa-shopping-cart"></em><?php echo toolsPny::getStaticRessource('customer_support', $language, $tabLabelsFront) ; ?></a></nav>
+                    <ul>
+                        <li><a target="_self" href="mailto:sales@pny.eu" class="block gold-icon"><em aria-hidden="true" class="fa fa-envelope"></em> <?php echo toolsPny::getStaticRessource('email_us', $language, $tabLabelsFront) ; ?></a></li>
+                    </ul>
+                </div>
+                <div class="lower-footer x-sm-12 sm-6 med-4 lg-4 column"> <nav> <a class="header" href="/support/technical-support"><em aria-hidden="true" class="fa fa-headphones"></em> Support</a> </nav>
+                    <ul>
+
+                        <li>     <a href="/support/glossary"><?php echo toolsPny::getStaticRessource('support_glossary', $language, $tabLabelsFront) ; ?></a>     </li>
+                        <li>     <a href="/support/contact-us"><?php echo toolsPny::getStaticRessource('contact_us', $language, $tabLabelsFront) ; ?></a>     </li>
+
+                    </ul>
+                </div>
+                <div class="lower-footer x-sm-12 sm-6 med-4 lg-4 column"> <nav> <a class="header" href="/company/about-pny"><em aria-hidden="true" class="fa fa-building"></em> <?php echo toolsPny::getStaticRessource('company_label', $language, $tabLabelsFront) ; ?></a></a></nav>
+                    <ul>
+                        <li>     <a href="/company/about-pny"><?php echo toolsPny::getStaticRessource('company_about', $language, $tabLabelsFront) ; ?></a>     </li>
+                        <li>     <a href="/company/press-center">Press Center</a>     </li>
+                        <li>     <a href="/company/press-center/events"><?php echo toolsPny::getStaticRessource('events', $language, $tabLabelsFront) ; ?></a>     </li>
+                        <li>     <a href="/company/careers"><?php echo toolsPny::getStaticRessource('company_careers', $language, $tabLabelsFront) ; ?></a>     </li>
+                        <li>     <a href="/company/where-to-buy"><?php echo toolsPny::getStaticRessource('where_to_buy', $language, $tabLabelsFront) ; ?></a>     </li>
+                        <li>     <a href="/company/contact-us"><?php echo toolsPny::getStaticRessource('contact_us', $language, $tabLabelsFront) ; ?></a>     </li>
+                    </ul>
+                </div>
+                <div class="lower-footer x-sm-12 sm-6 med-4 lg-4 column"> <nav> <a class="header" href="/company/about-pny"><em aria-hidden="true" class="fa fa-file-text-o"></em> <?php echo toolsPny::getStaticRessource('legal', $language, $tabLabelsFront) ; ?></a> </nav>
+                    <ul>
+                        <li>     <a href="/legal/privacy"><?php echo toolsPny::getStaticRessource('your_privacy', $language, $tabLabelsFront) ; ?></a>     </li>
+                        <li>     <a href="/legal/terms-of-use"><?php echo toolsPny::getStaticRessource('terms_of_use', $language, $tabLabelsFront) ; ?></a>     </li>
+                        <!-- <li>     <a href="/support/order-support/terms-of-sale"><?php echo toolsPny::getStaticRessource('contact_us', $language, $tabLabelsFront) ; ?></a>     </li>-->
+                    </ul>
+                </div>
+                <div class="clear-both">&nbsp;</div>
+                <div class="lower-footer sm-12 med-12 lg-12 column">
+                    <p class="lower-footer footnote"> Copyright © <?php echo date('Y');?> PNY Technologies Inc. All rights reserved. </p>
+                </div>
+
+</div></div></footer>
 
