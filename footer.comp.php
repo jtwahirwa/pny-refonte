f1dc48672c06ec98b94dc80ec0deac730908b232
TWAHIRWA <twahirwa@outlook.fr>
2018-09-05 17:02:16 +0200

    Add files via upload

--- footer.comp.php
+++ footer.comp.php
@@ -1,16 +1,12 @@
-            <link rel="stylesheet" type="text/css" href="/design/css/footer.css">
+
+    <footer class="main-footer" role="contentinfo">
+
+        <div class="footer-top-bar"></div>
 
 
+        <div class="footer-banner">
 
-
-
-<footer class="main-footer" role="contentinfo">
-
-    <div class="footer-top-bar"></div>
-
-
-     <div class="footer-banner">
-        <div class="social-foot-section row">
+            <div class="social-foot-section row center-footer">
                 <div class="lg-8 column no-padd pnycompany">
                     <h5 class="social-header">PNY Europe</h5>
                     <a href="https://www.facebook.com/pnyeuropemea"><img class="pny-icon" src="/data/social-medias/facebook.svg" alt="Facebook"></a>
@@ -36,47 +32,56 @@
 
 
 
-            <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-            <div class="row">
-                <div class="lower-footer x-sm-12 sm-6 med-4 lg-4 column"> <nav> <a class="header" href="/support/technical-support"><em aria-hidden="true" class="fa fa-wrench"></em> <?php echo toolsPny::getStaticRessource('technical_support', $language, $tabLabelsFront) ; ?></a> </nav>
-                    <ul>
-                        <li>     <a href="/support/support-form"><em aria-hidden="true" class="fa fa-envelope"></em> <?php echo toolsPny::getStaticRessource('contact_us', $language, $tabLabelsFront) ; ?></a>     </li>
-                    </ul>
-                </div>
-                <div class=" lower-footer x-sm-12 sm-6 med-4 lg-4 column"> <nav> <a class="header" href="/support/technical-support"><em aria-hidden="true" class="fa fa-shopping-cart"></em><?php echo toolsPny::getStaticRessource('customer_support', $language, $tabLabelsFront) ; ?></a></nav>
-                    <ul>
-                        <li><a target="_self" href="mailto:sales@pny.eu" class="block gold-icon"><em aria-hidden="true" class="fa fa-envelope"></em> <?php echo toolsPny::getStaticRessource('email_us', $language, $tabLabelsFront) ; ?></a></li>
-                    </ul>
-                </div>
-                <div class="lower-footer x-sm-12 sm-6 med-4 lg-4 column"> <nav> <a class="header" href="/support/technical-support"><em aria-hidden="true" class="fa fa-headphones"></em> Support</a> </nav>
-                    <ul>
 
-                        <li>     <a href="/support/glossary"><?php echo toolsPny::getStaticRessource('support_glossary', $language, $tabLabelsFront) ; ?></a>     </li>
-                        <li>     <a href="/support/contact-us"><?php echo toolsPny::getStaticRessource('contact_us', $language, $tabLabelsFront) ; ?></a>     </li>
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+         <div class="center-footer row">
+            <div class="lower-footer x-sm-12 sm-6 med-4 lg-4 column "> <nav> <a class="header" href="/support/technical-support"><em aria-hidden="true" class="fa fa-wrench"></em> <?= toolsPny::getStaticRessource('technical_support', $language, $tabLabelsFront) ; ?></a> </nav>
+                <ul>
+                    <li>     <a href="/support/support-form"><em aria-hidden="true" class="fa fa-envelope"></em> <?= toolsPny::getStaticRessource('contact_us', $language, $tabLabelsFront) ; ?></a>     </li>
+                </ul>
+            </div>
+            <div class=" lower-footer x-sm-12 sm-6 med-4 lg-4 column"> <nav> <a class="header" href="/support/technical-support"><em aria-hidden="true" class="fa fa-shopping-cart"></em><?= toolsPny::getStaticRessource('customer_support', $language, $tabLabelsFront) ; ?></a></nav>
+                <ul>
+                    <li><a target="_self" href="mailto:sales@pny.eu" class="block gold-icon"><em aria-hidden="true" class="fa fa-envelope"></em> <?= toolsPny::getStaticRessource('email_us', $language, $tabLabelsFront) ; ?></a></li>
+                </ul>
+            </div>
+            <div class="lower-footer x-sm-12 sm-6 med-4 lg-4 column"> <nav> <a class="header" href="/support/technical-support"><em aria-hidden="true" class="fa fa-headphones"></em> Support</a> </nav>
+                <ul>
 
-                    </ul>
-                </div>
-                <div class="lower-footer x-sm-12 sm-6 med-4 lg-4 column"> <nav> <a class="header" href="/company/about-pny"><em aria-hidden="true" class="fa fa-building"></em> <?php echo toolsPny::getStaticRessource('company_label', $language, $tabLabelsFront) ; ?></a></a></nav>
-                    <ul>
-                        <li>     <a href="/company/about-pny"><?php echo toolsPny::getStaticRessource('company_about', $language, $tabLabelsFront) ; ?></a>     </li>
-                        <li>     <a href="/company/press-center">Press Center</a>     </li>
-                        <li>     <a href="/company/press-center/events"><?php echo toolsPny::getStaticRessource('events', $language, $tabLabelsFront) ; ?></a>     </li>
-                        <li>     <a href="/company/careers"><?php echo toolsPny::getStaticRessource('company_careers', $language, $tabLabelsFront) ; ?></a>     </li>
-                        <li>     <a href="/company/where-to-buy"><?php echo toolsPny::getStaticRessource('where_to_buy', $language, $tabLabelsFront) ; ?></a>     </li>
-                        <li>     <a href="/company/contact-us"><?php echo toolsPny::getStaticRessource('contact_us', $language, $tabLabelsFront) ; ?></a>     </li>
-                    </ul>
-                </div>
-                <div class="lower-footer x-sm-12 sm-6 med-4 lg-4 column"> <nav> <a class="header" href="/company/about-pny"><em aria-hidden="true" class="fa fa-file-text-o"></em> <?php echo toolsPny::getStaticRessource('legal', $language, $tabLabelsFront) ; ?></a> </nav>
-                    <ul>
-                        <li>     <a href="/legal/privacy"><?php echo toolsPny::getStaticRessource('your_privacy', $language, $tabLabelsFront) ; ?></a>     </li>
-                        <li>     <a href="/legal/terms-of-use"><?php echo toolsPny::getStaticRessource('terms_of_use', $language, $tabLabelsFront) ; ?></a>     </li>
-                        <!-- <li>     <a href="/support/order-support/terms-of-sale"><?php echo toolsPny::getStaticRessource('contact_us', $language, $tabLabelsFront) ; ?></a>     </li>-->
-                    </ul>
-                </div>
-                <div class="clear-both">&nbsp;</div>
-                <div class="lower-footer sm-12 med-12 lg-12 column">
-                    <p class="lower-footer footnote"> Copyright © <?php echo date('Y');?> PNY Technologies Inc. All rights reserved. </p>
-                </div>
+                    <li>     <a href="/support/glossary"><?= toolsPny::getStaticRessource('support_glossary', $language, $tabLabelsFront) ; ?></a>     </li>
+                    <li>     <a href="/support/contact-us"><?= toolsPny::getStaticRessource('contact_us', $language, $tabLabelsFront) ; ?></a>     </li>
 
-</div></div></footer>
+                </ul>
+            </div>
+            <div class="lower-footer x-sm-12 sm-6 med-4 lg-4 column"> <nav> <a class="header" href="/company/about-pny"><em aria-hidden="true" class="fa fa-building"></em> <?= toolsPny::getStaticRessource('company_label', $language, $tabLabelsFront) ; ?></a></a></nav>
+                <ul>
+                    <li>     <a href="/company/about-pny"><?= toolsPny::getStaticRessource('company_about', $language, $tabLabelsFront) ; ?></a>     </li>
+                    <li>     <a href="/company/press-center">Press Center</a>     </li>
+                    <li>     <a href="/company/press-center/events"><?= toolsPny::getStaticRessource('events', $language, $tabLabelsFront) ; ?></a>     </li>
+                    <li>     <a href="/company/careers"><?= toolsPny::getStaticRessource('company_careers', $language, $tabLabelsFront) ; ?></a>     </li>
+                    <li>     <a href="/company/where-to-buy"><?= toolsPny::getStaticRessource('where_to_buy', $language, $tabLabelsFront) ; ?></a>     </li>
+                    <li>     <a href="/company/contact-us"><?= toolsPny::getStaticRessource('contact_us', $language, $tabLabelsFront) ; ?></a>     </li>
+                </ul>
+            </div>
+            <div class="lower-footer x-sm-12 sm-6 med-4 lg-4 column"> <nav> <a class="header" href="/company/about-pny"><em aria-hidden="true" class="fa fa-file-text-o"></em> <?= toolsPny::getStaticRessource('legal', $language, $tabLabelsFront) ; ?></a> </nav>
+                <ul>
+                    <li>     <a href="/legal/privacy">  <?= toolsPny::getStaticRessource('your_privacy', $language, $tabLabelsFront) ; ?></a>     </li>
+                    <li>     <a href="/legal/terms-of-use"><?=  toolsPny::getStaticRessource('terms_of_use', $language, $tabLabelsFront) ; ?></a>     </li>
+                    <li><a href="/legal/info-generale"> <?= toolsPny::getStaticRessource('info_generale_title', $language, $tabLabelsFront) ; ?></a></li>
+
+
+                    <!-- <li>     <a href="/support/order-support/terms-of-sale"><?= toolsPny::getStaticRessource('contact_us', $language, $tabLabelsFront) ; ?></a>     </li>-->
+                </ul>
+            </div>
+            <div class="clear-both">&nbsp;</div>
+            <div class="lower-footer sm-12 med-12 lg-12 column row">
+                <p class="lower-footer footnote"> Copyright © <?= date('Y');?> PNY Technologies Inc. All rights reserved. </p>
+            </div>
+
+        </div></div></footer>
+
+
+
+
+
 
